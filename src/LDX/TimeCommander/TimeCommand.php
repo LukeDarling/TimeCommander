19da092c7b3a0b61a4fa47a61191e4b27eb100fa
CookieCode <etienne.crosnier.la@gmail.com>
2018-01-29 02:14:08 +0100

    API Update

--- src/LDX/TimeCommander/TimeCommand.php
+++ src/LDX/TimeCommander/TimeCommand.php
@@ -1,20 +1,22 @@
 <?php
 namespace LDX\TimeCommander;
+
 use pocketmine\scheduler\PluginTask;
 use pocketmine\Server;
+
 class TimeCommand extends PluginTask {
-  public function __construct($plugin,$cmd) {
-    $this->plugin = $plugin;
-    $this->cmd = $cmd;
-    $this->start = false;
-    parent::__construct($plugin);
-  }
-  public function onRun($ticks) {
-    if($this->start) {
-      $this->plugin->runCommand($this->cmd);
-    } else {
-      $this->start = true;
-    }
-  }
+	public function __construct($plugin,$cmd) {
+		$this->plugin = $plugin;
+		$this->cmd = $cmd;
+		$this->start = false;
+		parent::__construct($plugin);
+	}
+	
+	public function onRun(int $currentTick) {
+		if($this->start) {
+			$this->plugin->runCommand($this->cmd);
+		}else{
+			$this->start = true;
+		}
+	}
 }
-?>
